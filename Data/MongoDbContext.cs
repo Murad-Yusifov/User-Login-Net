a8f3260d9cb5d8e1b970855302ba81f3aaec9acc
Muradddddd <muradyusifov969@gmail.com>
2026-04-24 20:59:07 +0400

    commit bad mongo db apply

--- Data/MongoDbContext.cs
+++ Data/MongoDbContext.cs
@@ -1,1 +1,15 @@
-git 
+using MongoDB.Driver;
+
+public class MongoDBContext
+{
+    private IMongoDatabase _database;
+
+    public MongoDBContext (IConfiguration config)
+    {
+        var client = new MongoClient(config["MongoDB:ConnectionString"]);
+        _database = client.GetDatabase(config["MongoDB:DatabaseName"]);
+    }
+
+    // public IMongoCollection<Comment> Comments => _database.GetCollection<Comment>("Comments");
+    // public IMongoCollection<Users> Users => _database.GetCollection<Users>("Users");
+}
